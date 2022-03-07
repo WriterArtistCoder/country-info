14ec3a9acfa71875ef102e22e9f445270a44a53b
League Student <league@iMac-12.attlocal.net>
2022-03-06 16:36:43 -0800

    Added world.csv, started writing SQL

--- src/main/java/com/tinystripz/country_info/CountryApplication.java
+++ src/main/java/com/tinystripz/country_info/CountryApplication.java
@@ -2,12 +2,26 @@ package com.tinystripz.country_info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @SpringBootApplication
 public class CountryApplication {
 
+	@Autowired
+	JdbcTemplate temp;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CountryApplication.class, args);
+		temp.execute("DROP TABLE world IF EXISTS");
+		temp.execute("CREATE TABLE world(
+			flag VARCHAR(128), iso_no SMALLINT,
+			iso2 VARCHAR(2), iso3 VARCHAR(3)
+			common_name VARCHAR(32),
+			formal_name VARCHAR(64),
+			un VARCHAR(20), recog INTEGER,
+			pop INTEGER, area SMALLINT");
+		
 	}
 
 }

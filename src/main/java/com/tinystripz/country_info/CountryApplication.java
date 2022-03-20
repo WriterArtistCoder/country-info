7d58e47391334c8294ac98b28765cedf8ef47a50
League Student <league@iMac-12.attlocal.net>
2022-03-20 16:35:37 -0700

    Fixed prev commit

--- src/main/java/com/tinystripz/country_info/CountryApplication.java
+++ src/main/java/com/tinystripz/country_info/CountryApplication.java
@@ -2,22 +2,25 @@ package com.tinystripz.country_info;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.jdbc.core.SpringTemplate;
 
 @SpringBootApplication
 public class CountryApplication {
-	
+
 	public static void main(String[] args) {
 		JdbcTemplate temp;
 
 		SpringApplication.run(CountryApplication.class, args);
 		temp.execute("DROP TABLE world IF EXISTS");
-		temp.execute("CREATE TABLE world(flag VARCHAR(128), iso_no SMALLINT, iso2 VARCHAR(2), iso3 VARCHAR(3), common_name VARCHAR(32), formal_name VARCHAR(64), un VARCHAR(20), recog INTEGER, pop INTEGER, area SMALLINT");
-		
-		 String currentPath = new java.io.File(".").getCanonicalPath();
+		temp.execute(
+				"CREATE TABLE world(flag VARCHAR(128), iso_no SMALLINT, iso2 VARCHAR(2), iso3 VARCHAR(3), common_name VARCHAR(32), formal_name VARCHAR(64), un VARCHAR(20), recog INTEGER, pop INTEGER, area SMALLINT");
+
+		String currentPath = new java.io.File(".").getCanonicalPath();
 		System.out.println("Current dir:" + currentPath);
 
 		Scanner s = new Scanner(new File("src/main/resources/world.csv"));
@@ -25,30 +28,39 @@ public class CountryApplication {
 			String[] values = s.nextLine().split(",");
 			String command = "INSERT INTO table_name VALUES (";
 
-			values.forEach(v->{
-				try { 
-					Integer.parseInt(v); 
-				} catch(NumberFormatException e) { 
+			Stream.of(values).forEach(v -> {
+				boolean isInt, isDouble;
+
+				try {
+					Integer.parseInt(v);
+				} catch (NumberFormatException e) {
 					isInt = false;
-				} catch(NullPointerException e) {
+				} catch (NullPointerException e) {
 					isInt = false;
 				}
 
-				try { 
-					Double.parseDouble(v); 
-				} catch(NumberFormatException e) { 
-					isDouble = false; 
-				} catch(NullPointerException e) {
+				try {
+					Double.parseDouble(v);
+				} catch (NumberFormatException e) {
+					isDouble = false;
+				} catch (NullPointerException e) {
 					isDouble = false;
 				}
+
+				if (isInt || isDouble) {
+					command += v + ", ";
+				} else {
+					command += "\"" + v + "\", ";
+				}
 			});
+
+			command = command.substring(command.length() - 2)
 		}
 
-		/* CONCERNS
-			Does not comply with SQL spec, just separates by newline then comma.
-			Auto-detects numbers and strings.
-			Does not have options to escape commas.
-		*/
+		/*
+		 * CONCERNS Does not comply with SQL spec, just separates by newline then comma.
+		 * Auto-detects numbers and strings. Does not have options to escape commas.
+		 */
 	}
 
 }
